6069f30ce597cfa1cf9efd466b1a227d1e08f92c
NicolaBellini <bellininicola4366@gmail.com>
2024-02-29 14:38:20 +0100

    [add] html css elements

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const canva = document.querySelector(".canva")
+// faccio il reset della mia tela eliminando qualsiasi cosa abbia messo nell html
+reset();
+
+
+
+console.log("ciao")
+
+
+// functions/////
+
+function reset(){
+  canva.innerHTML = ""
+}
